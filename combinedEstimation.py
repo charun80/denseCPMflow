input normalization without input overwriting; presumably correct scaling of epicflow

combinedEstimation.py
@@ -29,16 +29,36 @@ _denseCPMFlowResStruct = namedtuple("denseCPMFlowRes", "matches flowfield variPa
 
 
 def estimateFlow( img1, img2, img1Edges=None, nCPMSteps=3, variParams=None, epicParams=None, epicParamMode=None ):
-        
-    if img1Edges is None:
-        img1Edges = epic.computeSobelEdges( img1 )
-        allowEdgeModification = True
-    else:
-        allowEdgeModification = False
-    
-    img1 = np.ascontiguousarray( img1, dtype=np.float32 )
-    img2 = np.ascontiguousarray( img2, dtype=np.float32 )
 
+    imgS = np.ascontiguousarray( img1, dtype=np.float32 )
+    imgT = np.ascontiguousarray( img2, dtype=np.float32 )
+
+    mval = max( imgS.max(), imgT.max() )
+    if 1 < mval:
+        # normalization
+        nval = 1. / float(mval)
+        
+        if imgS is img1:
+            imgS = imgS * nval
+        else:
+            imgS *= nval
+        
+        if imgT is img2:
+            imgT = imgT * nval
+        else:
+            imgT *= nval
+    
+    #######
+    
+    if img1Edges is None:
+        imgSEdges = epic.computeSobelEdges( imgS ) * 255.
+    else:
+        imgSEdges = np.ascontiguousarray( img1Edges, dtype=np.float32 ) * 255.
+    
+    if imgSEdges is img1Edges:
+        allowEdgeModification = False
+    else:
+        allowEdgeModification = True
     
     #######
     
@@ -61,8 +81,8 @@ def estimateFlow( img1, img2, img1Edges=None, nCPMSteps=3, variParams=None, epic
     
     #######
     
-    lMatches = cpm.computeCPMFlow( img1, img2, n_steps=nCPMSteps )
-    flowField = epic.computeEpicFlow( img1, img2, img1Edges, lMatches, variParams, epicParams, allowEdgeModification )
+    lMatches = cpm.computeCPMFlow( imgS, imgT, n_steps=nCPMSteps )
+    flowField = epic.computeEpicFlow( imgS*255, imgT*255, imgSEdges, lMatches, variParams, epicParams, allowEdgeModification )
     
     
     return _denseCPMFlowResStruct( matches=lMatches, flowfield=flowField, variParams=variParams, epicParams=epicParams, nCPMSteps=nCPMSteps )